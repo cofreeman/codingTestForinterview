0e797a5d66d277900a640045f968bbb8d1e3da6b
choijungwoo493 <jwoo1016@gmail.com>
2022-11-09 01:32:31 +0900

    sds

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+    public static double solution(int[] arr){
+        LinkedList<Integer> integers = new LinkedList<>();
+        Arrays.stream(arr).forEach(integers::add);
+        boolean go = true;
+        int result = 0;
+        while(go){
+            for (int i = 0; i < ; i++) {
+                
+            }
+        }
+    }
+}
